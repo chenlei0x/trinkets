d911fb15346ed859142bbc7ce5c2d8e7a6e14be7
Larry Chen <lchen@suse.com>
2017-11-09 20:32:37 +0800

    tmux.sh

---
+++ misc/setup_suse/tmux.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+repo_alias=tmux_repo
+repo_uri=https://download.opensuse.org/repositories/utilities/openSUSE_Leap_42.3/
+tmux=tmux
+sudo zypper ar ${repo_uri} ${repo_alias}
+sudo zypper ref ${repo_alias}
+sudo zypper in --repo ${repo_alias} $tmux
+sudo zypper mr --disable $repo_alias
