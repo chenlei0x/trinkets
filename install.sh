c435d660172bd3ce853573260017e4cc13f42cef
Larry Chen <lchen@suse.com>
2017-12-20 20:28:06 +0800

    drop ugly dir comparison

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #! /bin/bash
 cd $(dirname $0)
 pwd=$PWD
-user_bin=$(cd ~/bin && pwd)
-if [ $pwd == $user_bin ]
+user_bin=~/bin
+
+#look how ugly it is!!!
+#user_bin=$(cd ~/bin && pwd)
+#if [ $pwd == $user_bin ]
+
+[ ! -e $user_bin ] && mkdir $user_bin
+if [ $user_bin -ef $pwd ]
 then
 	echo "in \$HOME/bin"
 	exit
 fi
 for i in $(find $pwd -maxdepth 1 -executable -type f)
 do
-	ln -s  $i ~/bin/
+	ln -s  $i $user_bin
 done
 
 
